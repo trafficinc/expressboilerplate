eecf44df3ba18d2c2018f930681aeb4157e9ab16
Ronnie B <trafficinc@users.noreply.github.com>
2017-09-22 10:53:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var express = require('express')
   , port = process.env.PORT || 3000
 
 app.set('views', __dirname + '/views')
-// app.engine('jade', require('jade').__express)
-// app.set('view engine', 'jade')
-
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'))
